326a680f8560fad94590801f5fd387771ff59e19
woutwoot <woutwoot13@gmail.com>
2014-10-25 13:39:44 +0200

    Better way to handle registering commands and config, added heal and feed command.
    Messages still have to be added.

--- src/main/java/net/doubledoordev/d3commands/D3Commands.java
+++ src/main/java/net/doubledoordev/d3commands/D3Commands.java
@@ -9,6 +9,7 @@ import net.doubledoordev.d3core.util.ID3Mod;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.common.config.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static net.doubledoordev.d3commands.util.Constants.MODID;
@@ -20,11 +21,8 @@ public class D3Commands implements ID3Mod
     @Mod.Instance(MODID)
     public static D3Commands instance;
 
-    private boolean tps =   true;
-    private boolean tpx =   true;
-    private boolean top =   true;
-    private boolean kill =  true;
-    private boolean getuuid = true;
+    private List<CommandEntry> commands = new ArrayList<>();
+
     public Configuration configuration;
 
     @Mod.EventHandler
@@ -41,11 +39,13 @@ public class D3Commands implements ID3Mod
         configuration.addCustomCategoryComment(MODID, "Set any value to false to disable the command.");
         configuration.setCategoryRequiresWorldRestart(MODID, true);
 
-        tps = configuration.getBoolean("tps", MODID, tps, "A TPS command for all players, not just ops.");
-        tpx = configuration.getBoolean("tpx", MODID, tpx, "Interdimensional TP command.");
-        top = configuration.getBoolean("top", MODID, top, "Teleport yourself to the highest block above you.");
-        kill = configuration.getBoolean("kill", MODID, kill, "Allow you to kill other players.");
-        getuuid = configuration.getBoolean("getuuid", MODID, getuuid, "Allows easy UUID grabbing.");
+        commands.add(new CommandEntry(new CommandTps(), configuration.getBoolean("tps", MODID, true, "A TPS command for all players, not just ops.")));
+        commands.add(new CommandEntry(new CommandTpx(), configuration.getBoolean("tpx", MODID, true, "Interdimensional TP command.")));
+        commands.add(new CommandEntry(new CommandTop(), configuration.getBoolean("top", MODID, true, "Teleport yourself to the highest block above you.")));
+        commands.add(new CommandEntry(new CommandKill(), configuration.getBoolean("kill", MODID, true, "Allow you to kill other players.")));
+        commands.add(new CommandEntry(new CommandHeal(), configuration.getBoolean("heal", MODID, true, "Heal yourself or other players.")));
+        commands.add(new CommandEntry(new CommandFeed(), configuration.getBoolean("feed", MODID, true, "Feed yourself or other players.")));
+        commands.add(new CommandEntry(new CommandGetUUID(), configuration.getBoolean("getuuid", MODID, true, "Allows easy UUID grabbing.")));
 
         if (configuration.hasChanged()) configuration.save();
     }
@@ -59,11 +59,11 @@ public class D3Commands implements ID3Mod
     @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent event)
     {
-        if (tps)        event.registerServerCommand(new CommandTps());
-        if (tpx)        event.registerServerCommand(new CommandTpx());
-        if (top)        event.registerServerCommand(new CommandTop());
-        if (kill)       event.registerServerCommand(new CommandKill());
-        if (getuuid)    event.registerServerCommand(new CommandGetUUID());
+        for(CommandEntry e : commands){
+            if(e.isEnabled()){
+                event.registerServerCommand(e.getCommand());
+            }
+        }
     }
 }
 
